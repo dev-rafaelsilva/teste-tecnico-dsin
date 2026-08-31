63a840412c77077421831ab059c8bb38711ce104
Rafael Silva <rafaelp_silva01@hotmail.com>
2026-08-31 16:50:32 -0300

    feat(q12): implementa algoritmo do pato

--- src/main/java/q12/Pato.java
+++ src/main/java/q12/Pato.java
@@ -1,4 +1,23 @@
 package q12;
 
 public class Pato {
+    private String nome;
+
+    public Pato(String nome) {
+        this.nome = nome;
+    }
+
+    public void grasnar() {
+        System.out.println(nome + " diz: Quack quack!");
+    }
+
+    public void nadar() {
+        System.out.println(nome + " está nadando na lagoa.");
+    }
+
+    public static void main(String[] args) {
+        Pato donald = new Pato("Donald");
+        donald.grasnar();
+        donald.nadar();
+    }
 }
